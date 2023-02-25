44af347d665b5b8b9d24df67d1e0db2fd916847c
Muhammad SA <MuhammadSA211999>
2023-02-25 17:38:16 +0600

    apply filters by status and colors

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,32 @@ import Todo from "./Todo";
 
 export default function TodoList() {
     const todos = useSelector((state) => state.todos)
+    const { status, colors } = useSelector((state) => state.filter)
+    const statusFilter = (todo) => {
+        switch (status) {
+            case 'complete':
+                return todo.completed
+            case 'incomplete':
+                return !todo.completed
+            default:
+                return true
+        }
+    }
+    const colorsFilter = (todo) => {
+        if (colors.length > 0) {
+            return colors.includes(todo?.color)
+        }
+        return true
+    }
+
     return (
         <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
             {
-                todos.map(todo => <Todo todo={todo} key={todo.id} />)
+                // filter todos for status property
+                todos.filter(statusFilter)
+                    // filter todos arrya for color
+                    .filter(colorsFilter)
+                    .map(todo => <Todo todo={todo} key={todo.id} />)
             }
 
         </div>
